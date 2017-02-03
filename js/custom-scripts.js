68a24c7c21aec0db4420f75be53f23e7b3aafd39
bIropka <biropka@gmail.com>
2017-02-04 00:01:04 +0200

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -12,11 +12,23 @@ $(document).ready(function () {
      ******* clicks scripts
      ******************************************************************************************************************/
 
+    $('.burger').click(function() {
+
+        $(this).parents('nav').toggleClass('active');
+
+    });
 
 
     /******************************************************************************************************************
      ******* slider scripts
      ******************************************************************************************************************/
+
+    $('.slider-banner').slick({
+        appendArrows: '.banner-control',
+        prevArrow: '.banner-prev',
+        nextArrow: '.banner-next',
+        dots: true
+    });
 
     $('.slider-news').slick({
         appendArrows: '.news-control',
@@ -30,7 +42,31 @@ $(document).ready(function () {
         appendArrows: '.products-control',
         prevArrow: '.products-prev',
         nextArrow: '.products-next',
-        slidesToShow: 4
+        slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 1231,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+             {
+             breakpoint: 641,
+             settings: {
+             slidesToShow: 2,
+             slidesToScroll: 2
+             }
+             },
+             {
+             breakpoint: 481,
+             settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+             }
+             }
+
+        ]
     });
 
 
